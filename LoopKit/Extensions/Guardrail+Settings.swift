8e2141593874743d0b927a94d7607daa25557c66
Tsviki <tsviki@gmail.com>
2024-10-30 00:08:19 +0200

    Update Guardrail+Settings.swift

--- LoopKit/Extensions/Guardrail+Settings.swift
+++ LoopKit/Extensions/Guardrail+Settings.swift
@@ -23,7 +23,7 @@ public extension Guardrail where Value == HKQuantity {
         .min()!
     }
 
-    static let correctionRange = Guardrail(absoluteBounds: (86.1)...(180.5), recommendedBounds: (99.1)...(115.9), unit: .milligramsPerDeciliter, startingSuggestion: 100)
+    static let correctionRange = Guardrail(absoluteBounds: (80.1)...(180.5), recommendedBounds: (99.1)...(115.9), unit: .milligramsPerDeciliter, startingSuggestion: 100)
 
     static func minCorrectionRangeValue(suspendThreshold: GlucoseThreshold?) -> HKQuantity {
         return [
